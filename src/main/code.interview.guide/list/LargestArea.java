621c84126af783ce28a65774892bba202a24ccf4
lichanghui <lichanghui@xiaomi.com>
2020-08-30 11:58:19 +0800

    2020-08-30 矩阵最大面积

--- src/main/code.interview.guide/list/LargestArea.java
+++ src/main/code.interview.guide/list/LargestArea.java
@@ -1,9 +1,17 @@
 package list;
 
 import java.util.Stack;
-
+/**
+ * 矩形最大面积。---重要的题。
+ * 需要注意的点：Stack==null的时候特殊情况处理。
+ * 计算的时候需要找到左右两边两个端点。
+ * */
 public class LargestArea {
-    public int largestRectangleArea(int[] heights) {
+    public static void main(String[] args) {
+        int[] nums = {5,4,2,1};
+        System.out.println(largestRectangleArea(nums));
+    }
+    public static int largestRectangleArea(int[] heights) {
         if (heights==null) return 0;
         if (heights.length==1) return heights[0];
         Stack<Integer> s = new Stack<Integer>();
@@ -23,13 +31,35 @@ public class LargestArea {
             }
         }
         if (!s.isEmpty()) {
-            int tempvalue = s.peek();
-            tempres =0;
+            while(!s.isEmpty()){
+                int tempvalue = s.pop();
+//                res = Math.max(res,heights[tempvalue]);
+                if(s.isEmpty()){
+                    res = Math.max(res,heights[tempvalue]*(heights.length));
+                }else{
+                    res = Math.max(res,heights[tempvalue]*(heights.length-s.peek()-1));
+                }
+
+            }
         }
         return res;
     }
 
-    private int findStackArea(Stack<Integer> s, int[] heights, int i) {
-        return 0;
+    private static int findStackArea(Stack<Integer> s, int[] heights, int i) {
+        int temp = s.pop();
+        int tempres = heights[temp];
+        if(s.isEmpty()){
+            tempres = Math.max(tempres,heights[temp]*(temp+1));
+        }
+        while(!s.isEmpty()&&heights[s.peek()] > heights[i]){
+            int temp2 = s.pop();
+            if(s.isEmpty()){
+                tempres = Math.max(tempres,heights[temp2]*(temp+1));
+            }else{
+                tempres = Math.max(tempres,heights[temp2]*(i-(s.peek()+1)));
+            }
+        }
+        s.push(i);
+        return tempres;
     }
 }
